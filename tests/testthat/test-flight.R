39e752161fa7575ac1b37499a1a5d4909f1b32da
Rémi Thériault <13123390+rempsyc@users.noreply.github.com>
2025-11-09 17:03:54 -0500

    remove python code so can install properly (works)

--- tests/testthat/test-flight.R
+++ tests/testthat/test-flight.R
@@ -1,23 +1,32 @@
 test_that("Flight object is created", {
-  flight <- Flight("2023-07-20", "JFKIST", "9:00AM", "5:00PM", 
-                   "8 hr 0 min", "Nonstop", "150 kg CO2", "10% emissions", "$450")
-  
+  flight <- Flight(
+    "2023-07-20",
+    "JFKIST",
+    "9:00AM",
+    "5:00PM",
+    "8 hr 0 min",
+    "Nonstop",
+    "150 kg CO2",
+    "10% emissions",
+    "$450"
+  )
+
   expect_s3_class(flight, "Flight")
   expect_equal(flight$date, "2023-07-20")
 })
 
 test_that("Flight parses origin and destination", {
   flight <- Flight("2023-07-20", "JFKIST")
-  
+
   expect_equal(flight$origin, "JFK")
   expect_equal(flight$dest, "IST")
 })
 
 test_that("Flight parses price correctly", {
   flight <- Flight("2023-07-20", "JFKIST", "$450")
-  
+
   expect_equal(flight$price, 450)
-  
+
   flight2 <- Flight("2023-07-20", "JFKIST", "$1,250")
   expect_equal(flight2$price, 1250)
 })
@@ -25,33 +34,33 @@ test_that("Flight parses price correctly", {
 test_that("Flight parses stops correctly", {
   flight1 <- Flight("2023-07-20", "JFKIST", "Nonstop")
   expect_equal(flight1$num_stops, 0)
-  
+
   flight2 <- Flight("2023-07-20", "JFKIST", "1 stop")
   expect_equal(flight2$num_stops, 1)
-  
+
   flight3 <- Flight("2023-07-20", "JFKIST", "2 stops")
   expect_equal(flight3$num_stops, 2)
 })
 
 test_that("Flight parses CO2 emissions", {
   flight <- Flight("2023-07-20", "JFKIST", "150 kg CO2", "10% emissions")
-  
+
   expect_equal(flight$co2, 150)
   expect_equal(flight$emissions, 10)
 })
 
 test_that("Flight parses flight time", {
   flight <- Flight("2023-07-20", "JFKIST", "8 hr 30 min")
-  
+
   expect_equal(flight$flight_time, "8 hr 30 min")
 })
 
 test_that("flights_to_dataframe creates data frame", {
   flight1 <- Flight("2023-07-20", "JFKIST", "$450", "Nonstop")
   flight2 <- Flight("2023-07-21", "ISTCDG", "$300", "1 stop")
-  
+
   df <- flights_to_dataframe(list(flight1, flight2))
-  
+
   expect_s3_class(df, "data.frame")
   expect_equal(nrow(df), 2)
   expect_true("origin" %in% names(df))
--- tests/testthat/test-scrape.R
+++ tests/testthat/test-scrape.R
@@ -1,6 +1,6 @@
 test_that("One-way trip is created correctly", {
   res <- Scrape("FCO", "IST", "2023-12-05")
-  
+
   expect_s3_class(res, "Scrape")
   expect_equal(res$origin[[1]], "FCO")
   expect_equal(res$dest[[1]], "IST")
@@ -10,7 +10,7 @@ test_that("One-way trip is created correctly", {
 
 test_that("Round-trip is created correctly", {
   res <- Scrape("LGA", "RDU", "2023-05-15", "2023-06-15")
-  
+
   expect_s3_class(res, "Scrape")
   expect_equal(res$origin[[1]], "LGA")
   expect_equal(res$dest[[1]], "RDU")
@@ -22,8 +22,18 @@ test_that("Round-trip is created correctly", {
 })
 
 test_that("Chain-trip is created correctly", {
-  res <- Scrape("JFK", "AMS", "2023-11-10", "CDG", "AMS", "2023-11-17", "AMS", "IST", "2023-11-25")
-  
+  res <- Scrape(
+    "JFK",
+    "AMS",
+    "2023-11-10",
+    "CDG",
+    "AMS",
+    "2023-11-17",
+    "AMS",
+    "IST",
+    "2023-11-25"
+  )
+
   expect_s3_class(res, "Scrape")
   expect_equal(unlist(res$origin), c("JFK", "CDG", "AMS"))
   expect_equal(unlist(res$dest), c("AMS", "AMS", "IST"))
@@ -32,12 +42,25 @@ test_that("Chain-trip is created correctly", {
 })
 
 test_that("Perfect-chain is created correctly", {
-  res <- Scrape("JFK", "2023-11-10", "AMS", "2023-11-17", "CDG", "2023-11-20", "IST", "2023-11-25", "JFK")
-  
+  res <- Scrape(
+    "JFK",
+    "2023-11-10",
+    "AMS",
+    "2023-11-17",
+    "CDG",
+    "2023-11-20",
+    "IST",
+    "2023-11-25",
+    "JFK"
+  )
+
   expect_s3_class(res, "Scrape")
   expect_equal(unlist(res$origin), c("JFK", "AMS", "CDG", "IST"))
   expect_equal(unlist(res$dest), c("AMS", "CDG", "IST", "JFK"))
-  expect_equal(unlist(res$date), c("2023-11-10", "2023-11-17", "2023-11-20", "2023-11-25"))
+  expect_equal(
+    unlist(res$date),
+    c("2023-11-10", "2023-11-17", "2023-11-20", "2023-11-25")
+  )
   expect_equal(res$type, "perfect-chain")
 })
 
